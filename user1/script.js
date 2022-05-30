73519640e85ff4145441fddbb6b04f53fc3fd24b
Sanket <ee19btech11012@iith.ac.in>
2022-05-31 00:58:58 +0530

    chat properly working

--- user1/script.js
+++ user1/script.js
@@ -1,36 +1,68 @@
 let offerButton = document.getElementById("create-offer");
+// all things will be initiated after offer creation
 offerButton.addEventListener("click", doTask, false);
+
+let connectionStatus = false;
 
 function doTask() {
 	const localConnection = new RTCPeerConnection();
-
+	// on getting new ice candidate
 	localConnection.onicecandidate = (e) => {
 		console.log(" NEW ice candidnat!! on localconnection reprinting SDP ");
 		console.log(JSON.stringify(localConnection.localDescription));
 		let offer = document.getElementById("offer-sdp");
+		// display the value in offer box
 		offer.value = JSON.stringify(localConnection.localDescription);
 	};
 
 	const sendChannel = localConnection.createDataChannel("sendChannel");
-	sendChannel.onmessage = (e) => console.log("messsage received!!!" + e.data);
-	sendChannel.onopen = (e) => console.log("open!!!!");
-	sendChannel.onclose = (e) => console.log("closed!!!!!!");
+	sendChannel.onmessage = (e) => {
+		console.log("messsage received!!!" + e.data);
+		let chatBox = document.getElementById("chat-history");
+		chatBox.innerHTML = `Message from user B:
+		 ${e.data}`;
+	};
+
+	// for putting system comments (user guidance)
+	let comments = document.getElementById("system-comments");
+
+	sendChannel.onopen = (e) => {
+		console.log("open!!!!");
+		connectionStatus = true;
+		comments.innerHTML = `System Comments: Whoaa!! Now you can chat with User 2.`;
+	};
+	sendChannel.onclose = (e) => {
+		console.log("closed!!!!!!");
+		connectionStatus = false;
+		comments.innerHTML = `System Comments: The channel is closed, you cannot chat.`;
+	};
 
 	localConnection
 		.createOffer()
 		.then((o) => localConnection.setLocalDescription(o));
 
-	let comments = document.getElementById("system-comments");
+	// ask user to put offer in other browser
 	comments.innerHTML = `System Comments: Paste this offer in the other browser.`;
 
 	let acceptOffer = document.getElementById("accept-offer");
 	acceptOffer.addEventListener("click", () => {
 		const answer = JSON.parse(document.getElementById("answer-sdp").value);
-		localConnection
-			.setRemoteDescription(answer)
-			.then((a) => console.log("done"));
+		localConnection.setRemoteDescription(answer).then((a) => {
+			console.log("done");
+		});
+	});
+
+	// functions for sending chat messages
+	document.getElementById("send-message").addEventListener("click", () => {
+		let messageBox = document.getElementById("message-box");
+		// sanity check for sending messages
+		if (
+			connectionStatus &&
+			messageBox.value != null &&
+			messageBox.value != ""
+		) {
+			sendChannel.send(messageBox.value);
+			messageBox.value = "";
+		}
 	});
 }
-
-// let temp = document.getElementById("chat-history");
-// temp.innerHTML = temp.innerHTML + `\n this is new text`;
--- user2/script.js
+++ user2/script.js
@@ -1,28 +1,49 @@
 let acceptOffer = document.getElementById("accept-offer");
+let connectionStatus = false;
+
+// process starts when user 2 accepts offer from user 1
 acceptOffer.addEventListener("click", async () => {
 	const offer = JSON.parse(document.getElementById("answer-sdp").value);
 	console.log(offer);
 	const remoteConnection = await new RTCPeerConnection();
 
+	// for putting system comments (user guidance)
+	let comments = document.getElementById("system-comments");
+
+	// on getting new ice candidate
 	remoteConnection.onicecandidate = (e) => {
 		console.log(" NEW ice candidnate!! on localconnection reprinting SDP ");
 		console.log(JSON.stringify(remoteConnection.localDescription));
 		const offerSDP = document.getElementById("offer-sdp");
+		// display the value in offer box
 		offerSDP.value = JSON.stringify(remoteConnection.localDescription);
+		comments.innerHTML = `System Comments: Paste the offer (left box) in other browser answer.`;
 	};
 
 	remoteConnection.ondatachannel = (e) => {
 		const receiveChannel = e.channel;
-		receiveChannel.onmessage = (e) =>
+		receiveChannel.onmessage = (e) => {
 			console.log("messsage received from A:" + e.data);
-		receiveChannel.onopen = (e) => console.log("open!!!!");
-		receiveChannel.onclose = (e) => console.log("closed!!!!!!");
+			let chatBox = document.getElementById("chat-history");
+			// display message from user A
+			chatBox.innerHTML = `Message from user A: ${e.data}`;
+		};
+		receiveChannel.onopen = (e) => {
+			console.log("open!!!!");
+			connectionStatus = true;
+			comments.innerHTML = `System Comments: Whoaa!! Now you can chat with User 1.`;
+		};
+		receiveChannel.onclose = (e) => {
+			console.log("closed!!!!!!");
+			connectionStatus = false;
+			comments.innerHTML = `System Comments: The channel is closed, you cannot chat.`;
+		};
 		remoteConnection.channel = receiveChannel;
 	};
 
-	await remoteConnection
-		.setRemoteDescription(offer)
-		.then((a) => console.log("done"));
+	await remoteConnection.setRemoteDescription(offer).then((a) => {
+		console.log("done");
+	});
 
 	//create answer
 	await remoteConnection
@@ -31,6 +52,18 @@ acceptOffer.addEventListener("click", async () => {
 		.then((a) =>
 			console.log(JSON.stringify(remoteConnection.localDescription))
 		);
-});
 
-//send the anser to the client
+	// functions for sending chat messages
+	document.getElementById("send-message").addEventListener("click", () => {
+		let messageBox = document.getElementById("message-box");
+		// sanity check for sending messages
+		if (
+			connectionStatus &&
+			messageBox.value != null &&
+			messageBox.value != ""
+		) {
+			remoteConnection.channel.send(messageBox.value);
+			messageBox.value = "";
+		}
+	});
+});
